59033fab478c889f3f2874ed21d81136d05f5b83
Moses A. Samuel <mosamuel98@gmail.com>
2019-08-23 09:59:32 -0700

    render character cards

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Tab, Menu, Icon } from "semantic-ui-react"
 import { NavLink } from "react-router-dom"
 
+import CharacterList from './CharacterList'
+
 // TODO: Add missing tabs below
 // Take a look at React Semantic UI tabs 
 // https://react.semantic-ui.com/modules/tab/
 
 const panes = [
-    { menuItem: 'Characters', render: () => <Tab.Pane>Characters</Tab.Pane> },
+    { menuItem: 'Characters', render: () => <Tab.Pane><CharacterList /></Tab.Pane> },
     { menuItem: 'Locations', render: () => <Tab.Pane>Locations</Tab.Pane> },
     { menuItem: 'Episodes', render: () => <Tab.Pane>Episodes</Tab.Pane> }
 ]
