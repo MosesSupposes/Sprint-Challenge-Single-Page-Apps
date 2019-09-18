aebc9c62acc7b34d95a1c7ba75680f34576c0d89
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-18 15:46:53 -0700

    render a welcome page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom'
 
 import TabNav from "./components/TabNav.js"
 import Header from "./components/Header.js"
+import WelcomePage from './components/WelcomePage'
 
 
 export default function App() {
   return (
     <main>
+      <WelcomePage />
       <Header />
       <TabNav />
 
